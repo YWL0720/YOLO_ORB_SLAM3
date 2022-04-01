018c90fed121476197c7090bf5c615eec190df11
YWL0720 <1074328870@qq.com>
2022-04-01 20:55:46 +0800

    v1.0

---
+++ include/YoloDetect.h
@@ -0,0 +1,36 @@
+//
+// Created by yuwenlu on 2022/3/14.
+//
+#ifndef YOLO_DETECT_H
+#define YOLO_DETECT_H
+
+#include <opencv2/opencv.hpp>
+#include <torch/script.h>
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <time.h>
+
+using namespace std;
+
+class YoloDetection
+{
+public:
+    YoloDetection();
+    ~YoloDetection();
+    void GetImage(cv::Mat& RGB);
+    void ClearImage();
+    bool Detect();
+    void ClearArea();
+    vector<cv::Rect2i> mvPersonArea = {};
+    vector<torch::Tensor> non_max_suppression(torch::Tensor preds, float score_thresh=0.5, float iou_thresh=0.5);
+
+public:
+    cv::Mat mRGB;
+    torch::jit::script::Module mModule;
+    std::vector<std::string> mClassnames;
+
+};
+
+
+#endif //YOLO_DETECT_H
---
+++ src/YoloDetect.cpp
@@ -0,0 +1,150 @@
+//
+// Created by yuwenlu on 2022/3/14.
+//
+#include <YoloDetect.h>
+
+YoloDetection::YoloDetection()
+{
+    mModule = torch::jit::load("yolov5s.torchscript.pt");
+
+    std::ifstream f("coco.names");
+    std::string name = "";
+    while (std::getline(f, name))
+    {
+        mClassnames.push_back(name);
+    }
+}
+
+YoloDetection::~YoloDetection()
+{
+
+}
+
+bool YoloDetection::Detect()
+{
+
+    cv::Mat img;
+
+    if(mRGB.empty())
+    {
+        std::cout << "Read RGB failed!" << std::endl;
+        return false;
+    }
+
+    // Preparing input tensor
+    cv::resize(mRGB, img, cv::Size(640, 380));
+    cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+    torch::Tensor imgTensor = torch::from_blob(img.data, {img.rows, img.cols,3},torch::kByte);
+    imgTensor = imgTensor.permute({2,0,1});
+    imgTensor = imgTensor.toType(torch::kFloat);
+    imgTensor = imgTensor.div(255);
+    imgTensor = imgTensor.unsqueeze(0);
+
+    // preds: [?, 15120, 9]
+    torch::Tensor preds = mModule.forward({imgTensor}).toTuple()->elements()[0].toTensor();
+    std::vector<torch::Tensor> dets = YoloDetection::non_max_suppression(preds, 0.4, 0.5);
+    if (dets.size() > 0)
+    {
+        // Visualize result
+        for (size_t i=0; i < dets[0].sizes()[0]; ++ i)
+        {
+            float left = dets[0][i][0].item().toFloat() * mRGB.cols / 640;
+            float top = dets[0][i][1].item().toFloat() * mRGB.rows / 384;
+            float right = dets[0][i][2].item().toFloat() * mRGB.cols / 640;
+            float bottom = dets[0][i][3].item().toFloat() * mRGB.rows / 384;
+            int classID = dets[0][i][5].item().toInt();
+
+            if (mClassnames[classID] == "person")
+            {
+                cv::Rect2i PersonArea(left, top, (right - left), (bottom - top));
+                mvPersonArea.push_back(PersonArea);
+            }
+
+        }
+        if (mvPersonArea.size() == 0)
+        {
+            cv::Rect2i tPersonArea(1, 1, 1, 1);
+            mvPersonArea.push_back(tPersonArea);
+        }
+    }
+    return true;
+}
+
+vector<torch::Tensor> YoloDetection::non_max_suppression(torch::Tensor preds, float score_thresh, float iou_thresh)
+{
+    std::vector<torch::Tensor> output;
+    for (size_t i=0; i < preds.sizes()[0]; ++i)
+    {
+        torch::Tensor pred = preds.select(0, i);
+
+        // Filter by scores
+        torch::Tensor scores = pred.select(1, 4) * std::get<0>( torch::max(pred.slice(1, 5, pred.sizes()[1]), 1));
+        pred = torch::index_select(pred, 0, torch::nonzero(scores > score_thresh).select(1, 0));
+        if (pred.sizes()[0] == 0) continue;
+
+        // (center_x, center_y, w, h) to (left, top, right, bottom)
+        pred.select(1, 0) = pred.select(1, 0) - pred.select(1, 2) / 2;
+        pred.select(1, 1) = pred.select(1, 1) - pred.select(1, 3) / 2;
+        pred.select(1, 2) = pred.select(1, 0) + pred.select(1, 2);
+        pred.select(1, 3) = pred.select(1, 1) + pred.select(1, 3);
+
+        // Computing scores and classes
+        std::tuple<torch::Tensor, torch::Tensor> max_tuple = torch::max(pred.slice(1, 5, pred.sizes()[1]), 1);
+        pred.select(1, 4) = pred.select(1, 4) * std::get<0>(max_tuple);
+        pred.select(1, 5) = std::get<1>(max_tuple);
+
+        torch::Tensor  dets = pred.slice(1, 0, 6);
+
+        torch::Tensor keep = torch::empty({dets.sizes()[0]});
+        torch::Tensor areas = (dets.select(1, 3) - dets.select(1, 1)) * (dets.select(1, 2) - dets.select(1, 0));
+        std::tuple<torch::Tensor, torch::Tensor> indexes_tuple = torch::sort(dets.select(1, 4), 0, 1);
+        torch::Tensor v = std::get<0>(indexes_tuple);
+        torch::Tensor indexes = std::get<1>(indexes_tuple);
+        int count = 0;
+        while (indexes.sizes()[0] > 0)
+        {
+            keep[count] = (indexes[0].item().toInt());
+            count += 1;
+
+            // Computing overlaps
+            torch::Tensor lefts = torch::empty(indexes.sizes()[0] - 1);
+            torch::Tensor tops = torch::empty(indexes.sizes()[0] - 1);
+            torch::Tensor rights = torch::empty(indexes.sizes()[0] - 1);
+            torch::Tensor bottoms = torch::empty(indexes.sizes()[0] - 1);
+            torch::Tensor widths = torch::empty(indexes.sizes()[0] - 1);
+            torch::Tensor heights = torch::empty(indexes.sizes()[0] - 1);
+            for (size_t i=0; i<indexes.sizes()[0] - 1; ++i)
+            {
+                lefts[i] = std::max(dets[indexes[0]][0].item().toFloat(), dets[indexes[i + 1]][0].item().toFloat());
+                tops[i] = std::max(dets[indexes[0]][1].item().toFloat(), dets[indexes[i + 1]][1].item().toFloat());
+                rights[i] = std::min(dets[indexes[0]][2].item().toFloat(), dets[indexes[i + 1]][2].item().toFloat());
+                bottoms[i] = std::min(dets[indexes[0]][3].item().toFloat(), dets[indexes[i + 1]][3].item().toFloat());
+                widths[i] = std::max(float(0), rights[i].item().toFloat() - lefts[i].item().toFloat());
+                heights[i] = std::max(float(0), bottoms[i].item().toFloat() - tops[i].item().toFloat());
+            }
+            torch::Tensor overlaps = widths * heights;
+
+            // FIlter by IOUs
+            torch::Tensor ious = overlaps / (areas.select(0, indexes[0].item().toInt()) + torch::index_select(areas, 0, indexes.slice(0, 1, indexes.sizes()[0])) - overlaps);
+            indexes = torch::index_select(indexes, 0, torch::nonzero(ious <= iou_thresh).select(1, 0) + 1);
+        }
+        keep = keep.toType(torch::kInt64);
+        output.push_back(torch::index_select(dets, 0, keep.slice(0, 0, count)));
+    }
+    return output;
+}
+
+void YoloDetection::GetImage(cv::Mat &RGB)
+{
+    mRGB = RGB;
+}
+
+void YoloDetection::ClearImage()
+{
+    mRGB = 0;
+}
+
+void YoloDetection::ClearArea()
+{
+    mvPersonArea.clear();
+}
